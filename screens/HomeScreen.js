ac04bedc03b7c286242cab3dd819cac58f648881
tuantm <nazik.vg@gmail.com>
2018-03-28 15:47:04 +0700

    image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View,StyleSheet,Text,Button}from'react-native';
+import {View,StyleSheet,Text,Button,Image}from'react-native';
 class HomeScreen extends Component{
     static navigationOptions = {
         title: 'HomeScreen',
@@ -10,6 +10,7 @@ class HomeScreen extends Component{
             <View style={{backgroundColor:'#ffffff',flex:1}}>
             <Text>Hello {params.name} Home</Text>
             <Button style={{backgroundColor:'#000000',width: 100,height: 40,}} onPress={()=>this.props.navigation.goBack()} title="Back to login"/>
+            <Image source={require("../images/background.jpg")}/>
             </View>
         );
     }
