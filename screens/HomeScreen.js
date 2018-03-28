7dd4b7337ee777d208498f07f663aa7296b88d2c
tuantm <nazik.vg@gmail.com>
2018-03-28 15:26:15 +0700

    add back button from home back to login screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View,StyleSheet,Text}from'react-native';
+import {View,StyleSheet,Text,Button}from'react-native';
 class HomeScreen extends Component{
     static navigationOptions = {
         title: 'HomeScreen',
@@ -8,6 +8,7 @@ class HomeScreen extends Component{
         return(
             <View style={{backgroundColor:'#ffffff',flex:1}}>
             <Text>Home</Text>
+            <Button style={{backgroundColor:'#000000',width: 100,height: 40,}} onPress={()=>this.props.navigation.goBack()} title="Back to login"/>
             </View>
         );
     }
